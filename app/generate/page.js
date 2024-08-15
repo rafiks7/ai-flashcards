35a119c6cb4cdedfc881008ef1e83de124122864
Andrew Shiroma <amshiroma@zoho.com>
2024-08-15 11:15:18 -0700

    changed color for popup buttons

--- app/generate/page.js
+++ app/generate/page.js
@@ -223,9 +223,6 @@ export default function Generate() {
         <Dialog open={open} onClose={handleClose}>
           <DialogTitle>Save Flashcard Set</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              Enter a name for your new flashcard set.
-            </DialogContentText>
             <TextField
               autoFocus
               margin="dense"
